1951e8f0eaedf41e2aab038eb7d2951b8fa610c9
Wagner Xu <wagnerhsu@hotmail.com>
2024-09-28 11:14:20 +0800

    feat: update files

--- MyCode/05-Array/lib/es6-functional.js
+++ MyCode/05-Array/lib/es6-functional.js
@@ -1,26 +1,24 @@
-const forEach = (array,fn) => {
-   let i;
-   for(i=0;i<array.length;i++)
-      fn(array[i])
-}
+const forEach = (array, fn) => {
+  let i;
+  for (i = 0; i < array.length; i++) fn(array[i]);
+};
 
-const forEachObject = (obj,fn) => {
-    for (var property in obj) {
-	    if (obj.hasOwnProperty(property)) {
-	    	//calls the fn with key and value as its argument
-	        fn(property, obj[property]) 
-	    }
+const forEachObject = (obj, fn) => {
+  for (var property in obj) {
+    if (obj.hasOwnProperty(property)) {
+      //calls the fn with key and value as its argument
+      fn(property, obj[property]);
     }
-}
+  }
+};
 
-const unless = (predicate,fn) => {
-	if(!predicate)
-		fn()
-}
+const unless = (predicate, fn) => {
+  if (!predicate) fn();
+};
 
 const times = (times, fn) => {
   for (var i = 0; i < times; i++) fn(i);
-}
+};
 
 /*
 ES5 implementation
@@ -32,111 +30,111 @@ const every = (arr,fn) => {
 }
 */
 
-const every = (arr,fn) => {
-    let result = true;
-    for(const value of arr)
-       result = result && fn(value)
-    return result
-}
+const every = (arr, fn) => {
+  let result = true;
+  for (const value of arr) result = result && fn(value);
+  return result;
+};
 
-const some = (arr,fn) => {
-    let result = false;
-    for(const value of arr)
-       result = result || fn(value)
-    return result
-}
+const some = (arr, fn) => {
+  let result = false;
+  for (const value of arr) result = result || fn(value);
+  return result;
+};
 
 const sortBy = (property) => {
-    return (a,b) => {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result;
-    }
-}
+  return (a, b) => {
+    var result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result;
+  };
+};
 
-const tap = (value) =>
-  (fn) => (
-    typeof(fn) === 'function' && fn(value),
-    console.log(value)
-  )
+const tap = (value) => (fn) => (
+  typeof fn === "function" && fn(value), console.log(value)
+);
 
-const unary = (fn) =>
-  fn.length === 1
-    ? fn
-    : (arg) => fn(arg) 
+const unary = (fn) => (fn.length === 1 ? fn : (arg) => fn(arg));
 
 const once = (fn) => {
   let done = false;
 
   return function () {
-    return done ? undefined : ((done = true), fn.apply(this, arguments))
-  }
-}
+    return done ? undefined : ((done = true), fn.apply(this, arguments));
+  };
+};
 
 const memoized = (fn) => {
   const lookupTable = {};
-    
+
   return (arg) => lookupTable[arg] || (lookupTable[arg] = fn(arg));
-}
+};
 
-const map = (array,fn) => {
-  let results = []
-  for(const value of array)
-      results.push(fn(value))
+const map = (array, fn) => {
+  let results = [];
+  for (const value of array) results.push(fn(value));
 
-  return results;  
-}
+  return results;
+};
 
-const filter = (array,fn) => {
-  let results = []
-  for(const value of array)
-     (fn(value)) ? results.push(value) : undefined
+const filter = (array, fn) => {
+  let results = [];
+  for (const value of array) fn(value) ? results.push(value) : undefined;
 
-  return results;  
-}
+  return results;
+};
 
-const concatAll = (array,fn) => {
-  let results = []
-  for(const value of array)
-     results.push.apply(results, value);
+const concatAll = (array, fn) => {
+  let results = [];
+  for (const value of array) results.push.apply(results, value);
 
-  return results;  
-}
+  return results;
+};
 
-const reduce = (array,fn,initialValue) => {
-	let accumlator;
+const reduce = (array, fn, initialValue) => {
+  let accumlator;
 
-	if(initialValue != undefined)
-		accumlator = initialValue;
-	else
-		accumlator = array[0];
+  if (initialValue != undefined) accumlator = initialValue;
+  else accumlator = array[0];
 
-	if(initialValue === undefined)
-		for(let i=1;i<array.length;i++)
-			accumlator = fn(accumlator,array[i])
-	else
-		for(const value of array)
-    		accumlator = fn(accumlator,value)
+  if (initialValue === undefined)
+    for (let i = 1; i < array.length; i++)
+      accumlator = fn(accumlator, array[i]);
+  else for (const value of array) accumlator = fn(accumlator, value);
 
-	return [accumlator]	
-}
+  return [accumlator];
+};
 
+const zip = (leftArr, rightArr, fn) => {
+  let index,
+    results = [];
 
-const zip = (leftArr,rightArr,fn) => {
-  let index, results = [];
+  for (index = 0; index < Math.min(leftArr.length, rightArr.length); index++)
+    results.push(fn(leftArr[index], rightArr[index]));
 
-  for(index = 0;index < Math.min(leftArr.length, rightArr.length);index++)
-    results.push(fn(leftArr[index],rightArr[index]));
-  
-  return results; 
-}
+  return results;
+};
 
 const arrayUtils = {
-  map : map,
-  filter : filter,
-  concatAll : concatAll,
-  flatten : concatAll,
-  reduce : reduce,
-  zip : zip
-}
+  map: map,
+  filter: filter,
+  concatAll: concatAll,
+  flatten: concatAll,
+  reduce: reduce,
+  zip: zip,
+};
 
-export {forEach, forEachObject, unless,times,sortBy,every,some,tap,unary,once,memoized,arrayUtils}
+export {
+  forEach,
+  forEachObject,
+  unless,
+  times,
+  sortBy,
+  every,
+  some,
+  tap,
+  unary,
+  once,
+  memoized,
+  arrayUtils,
+};
